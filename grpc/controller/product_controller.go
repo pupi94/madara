b4e2c4a803dee2756caf9a36b31079ad8cdf7ee2
huang puping <huangpuping@shoplazza.com>
2021-06-21 13:55:57 +0800

    调整 grpc 目录结构

--- grpc/controller/product_controller.go
+++ grpc/controller/product_controller.go
@@ -3,7 +3,7 @@ package controller
 import (
 	"context"
 	"github.com/golang/protobuf/ptypes/empty"
-	"github.com/pupi94/madara/grpc/pb"
+	"github.com/pupi94/madara/grpc/pb/v1"
 )
 
 type ProductController struct {
@@ -13,21 +13,21 @@ func NewProductController() *ProductController {
 	return &ProductController{}
 }
 
-func (controller *ProductController) CreateProduct(ctx context.Context, request *pb.CreateProductRequest) (*pb.ProductResponse, error) {
+func (controller *ProductController) CreateProduct(ctx context.Context, request *v1.CreateProductRequest) (*v1.ProductResponse, error) {
 	return nil, nil
 }
 
-func (controller *ProductController) DeleteProduct(ctx context.Context, request *pb.DeleteProductRequest) (*empty.Empty, error) {
+func (controller *ProductController) DeleteProduct(ctx context.Context, request *v1.DeleteProductRequest) (*empty.Empty, error) {
 	return nil, nil
 }
 
-func (controller *ProductController) UpdateProduct(ctx context.Context, req *pb.UpdateProductRequest) (*pb.ProductResponse, error) {
+func (controller *ProductController) UpdateProduct(ctx context.Context, req *v1.UpdateProductRequest) (*v1.ProductResponse, error) {
 	return nil, nil
 }
 
-func (controller *ProductController) GetProduct(ctx context.Context, req *pb.GetProductRequest) (*pb.ProductResponse, error) {
+func (controller *ProductController) GetProduct(ctx context.Context, req *v1.GetProductRequest) (*v1.ProductResponse, error) {
 	return nil, nil
 }
-func (controller *ProductController) ListProduct(ctx context.Context, req *pb.ListProductRequest) (*pb.ListProductResponse, error) {
+func (controller *ProductController) ListProduct(ctx context.Context, req *v1.ListProductRequest) (*v1.ListProductResponse, error) {
 	return nil, nil
 }
--- grpc/proto/gen.sh
+++ grpc/proto/gen.sh
@@ -1,10 +1,10 @@
 if [ ! -f "$GOPATH/bin/protoc-go-inject-tag" ];then go get github.com/favadi/protoc-go-inject-tag; fi
 
-protoc -I . -I=$(pwd)/grpc/imports  --proto_path=$(pwd)/grpc/proto --go_out=plugins=grpc:$(pwd)/grpc/pb $(pwd)/grpc/proto/product.proto
-protoc-go-inject-tag -input=$(pwd)/grpc/pb/product.pb.go
+protoc -I . -I=$(pwd)/grpc/imports  --proto_path=$(pwd)/grpc/proto/v1 --go_out=plugins=grpc:$(pwd)/grpc/pb/v1 $(pwd)/grpc/proto/v1/product.proto
+protoc-go-inject-tag -input=$(pwd)/grpc/pb/v1/product.pb.go
 
-protoc -I . -I=$(pwd)/grpc/imports  --proto_path=$(pwd)/grpc/proto --go_out=plugins=grpc:$(pwd)/grpc/pb $(pwd)/grpc/proto/common.proto
-protoc-go-inject-tag -input=$(pwd)/grpc/pb/common.pb.go
+protoc -I . -I=$(pwd)/grpc/imports  --proto_path=$(pwd)/grpc/proto/v1 --go_out=plugins=grpc:$(pwd)/grpc/pb/v1 $(pwd)/grpc/proto/v1/common.proto
+protoc-go-inject-tag -input=$(pwd)/grpc/pb/v1/common.pb.go
 
-protoc -I . -I=$(pwd)/grpc/imports  --proto_path=$(pwd)/grpc/proto --go_out=plugins=grpc:$(pwd)/grpc/pb $(pwd)/grpc/proto/api.proto
-protoc-go-inject-tag -input=$(pwd)/grpc/pb/api.pb.go
+protoc -I . -I=$(pwd)/grpc/imports  --proto_path=$(pwd)/grpc/proto/v1 --go_out=plugins=grpc:$(pwd)/grpc/pb/v1 $(pwd)/grpc/proto/v1/api.proto
+protoc-go-inject-tag -input=$(pwd)/grpc/pb/v1/api.pb.go
